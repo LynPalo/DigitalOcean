c8958084c55e36f5d39e036aac19df9a93b168c0
Sean <sean@Seans-MBP.fios-router.home>
2019-06-10 10:41:16 -0400

    load balancer

--- main.tf
+++ main.tf
@@ -5,4 +5,22 @@ resource "digitalocean_droplet" "web" {
     size = "512mb"
 }
 
+resource "digitalocean_loadbalancer" "public" {
+  name = "loadbalancer-1"
+  region = "nyc1"
 
+  forwarding_rule {
+    entry_port = 80
+    entry_protocol = "http"
+
+    target_port = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port = 22
+    protocol = "tcp"
+  }
+
+  droplet_ids = ["${digitalocean_droplet.web.id}"]
+}
